ca4a36954f998d1372d26f0f5338dc052b2a7543
dntngks30 <dntngks30@gmail.com>
2026-02-16 19:58:03 +0900

    chore: baseline expo init

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>무명의 노래</Text>
+    </View>
+  );
+}
